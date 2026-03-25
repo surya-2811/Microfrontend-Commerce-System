3fe8a69a6a7b3a2cd70c5a3b2843e2a576c51bb5
Surya-2811 <suryaroxx1999@gmail.com>
2026-03-26 00:06:54 +0530

    feat: Add initial Webpack Module Federation configurations for host, electronics, and groceries microfrontends.

--- apps/electronics/webpack.config.js
+++ apps/electronics/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
 
 module.exports = {
     mode: "development",
@@ -17,7 +18,10 @@ module.exports = {
         rules: [
             {
                 test: /\.jsx?$/,
-                exclude: /node_modules/,
+                include: [
+                    path.resolve(__dirname, "src"),
+                    path.resolve(__dirname, "../../packages/ui/src"),
+                ],
                 use: {
                     loader: "babel-loader",
                     options: {
